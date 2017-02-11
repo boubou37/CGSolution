ec00c39214510c280baeacfff8172e2d85c90504
boubou37 <lrahamef@student.concordia.ab.ca>
2017-02-11 23:59:33 +0100

    Added java test initializer

---
+++ cgjava/src/test/java/fr/home/codingame/TestInitializer.java
@@ -0,0 +1,34 @@
+package fr.home.codingame;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by lrhmf on 11/02/2017.
+ */
+public class TestInitializer {
+
+    public static List<Scanner> getTestsInput(String difficulty, String puzzleName) throws FileNotFoundException {
+        List<Scanner> ret = new ArrayList<Scanner>();
+        File f;
+        String testFolder = "";
+        URL testFolderURI = TestInitializer.class.getClassLoader().getResource(difficulty+"/"+puzzleName);
+        if (testFolderURI != null) {
+            testFolder = testFolderURI.getFile();
+        }
+        f = new File(testFolder);
+        if (!f.isDirectory() || f == null) {
+            return null;
+        }
+        for (File testcase : f.listFiles()) {
+            Scanner s = new Scanner(testcase);
+            ret.add(s);
+        }
+        return ret;
+    }
+}
--- cgjava/src/test/java/fr/home/codingame/community/MagicStoneTest.java
+++ cgjava/src/test/java/fr/home/codingame/community/MagicStoneTest.java
@@ -4,6 +4,9 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.net.URL;
+
 /**
  * Created by lrhmf on 11/02/2017.
  */
@@ -11,6 +14,9 @@ import org.junit.Test;
 public class MagicStoneTest extends TestCase {
     @Test
     public void testSolve() throws Exception {
+        String cl = getClass().getClassLoader().getResource("community/magicstones/Test0.txt").getFile();
+        File f = new File(cl);
+        System.out.println(f);
         Assert.assertTrue(true);
     }
 
