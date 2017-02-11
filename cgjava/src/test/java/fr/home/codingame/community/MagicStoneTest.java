31c1fd98e448ba89b1a46dcae72ddc79899acc9d
boubou37 <lrahamef@student.concordia.ab.ca>
2017-02-12 00:05:57 +0100

    Added BeforeClass and AfterClass methods for MagicStonesTests

--- cgjava/src/test/java/fr/home/codingame/community/MagicStoneTest.java
+++ cgjava/src/test/java/fr/home/codingame/community/MagicStoneTest.java
@@ -1,17 +1,36 @@
 package fr.home.codingame.community;
 
+import fr.home.codingame.TestInitializer;
 import junit.framework.TestCase;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
-import java.net.URL;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by lrhmf on 11/02/2017.
  */
 
 public class MagicStoneTest extends TestCase {
+
+    List<Scanner> testInputs;
+
+    @BeforeClass
+    public void loadStreamReaders() throws Exception {
+        testInputs = TestInitializer.getTestsInput("community","magicstones");
+    }
+
+    @AfterClass
+    public void clearReaders() {
+        for (Scanner ti : testInputs) {
+            ti.close();
+        }
+    }
+
     @Test
     public void testSolve() throws Exception {
         String cl = getClass().getClassLoader().getResource("community/magicstones/Test0.txt").getFile();
